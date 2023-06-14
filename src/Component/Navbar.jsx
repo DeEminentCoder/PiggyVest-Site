0e10fdaf23b73e0aceed54abc760c33bee04aa90
Kehinde Olaleye <simeonkenny66@gmail.com>
2023-06-14 14:13:59 +0100

    add hero component

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,14 +4,14 @@ const Navbar = () => {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-white shadow-sm py-3 px-5">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#"><img src="logo.svg" alt="" /></a>
+    <nav className="navbar navbar-expand-lg bg-body-white shadow-sm py-3">
+    <div className="container-fluid gap-2">
+      <a className="navbar-brand ms-5" href="#"><img src="logo.svg" alt="" /></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className="navbar-nav me-auto mt-2 gap-3 ">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Save</a>
           </li>
@@ -28,9 +28,9 @@ const Navbar = () => {
             <a className="nav-link">Resources</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <button className="btn btn-lg btn-outline-success" type="submit">Sign in</button>
-          <button className="btn btn-lg btn-outline-success" type="submit">Create free account</button>
+        <form className="d-flex me-5 gap-3" role="search">
+          <button className="btn btn-lg btn-outline-dark border" id='signInButt' type="submit">Sign in</button>
+          <button className="btn btn-lg btn-dark border" id='createAccntButt' type="submit">Create free account</button>
         </form>
       </div>
     </div>
