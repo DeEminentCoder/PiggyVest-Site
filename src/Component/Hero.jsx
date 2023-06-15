c4dfee6345e4365ed22d36fd584eaf8a9fd6240e
Kehinde Olaleye <simeonkenny66@gmail.com>
2023-06-15 21:52:31 +0100

    hero modified

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 function Hero() {
   return (
     <>
-    <div className='row container-fluid p-5'>
-        <div className='col-6' id='leftSideHero'>
-            <h1>The Better Way to Save & Invest.</h1>
+    <center className='row container-fluid' id='hero'>
+        <div className='col-12 col-sm-6 px-' id='leftSideHero'>
+            <span className='ps-'>The Better Way <br /> to Save & Invest.</span>
+            <h6 className='fs-'>
+              PiggyVest helps over 4 million customers achieve their financial goals by helping them save and invest with ease.
+            </h6>
+          <button className="col-sm- btn btn-lg btn-dark border mx-2 my-2" id='createAccntButt'>Create free account</button>
+
         </div>
-        <div className='col-6'>
-            <h1>The Better Way to Save & Invest.</h1>
+        <div className='col-12 col-sm-6 mt- container px-0'>
+            <img src="sssf.png" alt="" width={400}/>
         </div>
-    </div>
+    </center>
     </>
   )
 }
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse navbar-collapse " id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mt-2 gap-3 ">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Save</a>
